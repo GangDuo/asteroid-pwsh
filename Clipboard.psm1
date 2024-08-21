fc35a9c918c87a91f881301263434b30a0aeaffd
GangDuo <5295036+GangDuo@users.noreply.github.com>
2024-08-21 10:09:44 +0900

    Set-ClipboardWithImageを追加

---
+++ Clipboard.psm1
@@ -0,0 +1,10 @@
+﻿function Set-ClipboardWithImage {
+    param (
+        [Parameter(Mandatory)]
+        [string]$Path
+    )
+
+    $bmp = [System.Drawing.Bitmap]::new($Path)
+    [System.Windows.Forms.Clipboard]::SetImage($bmp)
+    $bmp.Dispose()
+}
