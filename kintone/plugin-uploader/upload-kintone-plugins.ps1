b02829f7b6d3e3f24bffea247b8f67bcc4b18e1f
GangDuo <5295036+GangDuo@users.noreply.github.com>
2024-09-12 11:21:25 +0900

    任意フォルダのすべてのプラグインファイルをkintoneに追加する。

---
+++ kintone/plugin-uploader/upload-kintone-plugins.ps1
@@ -0,0 +1,12 @@
+﻿# https://cybozu.dev/ja/kintone/sdk/development-environment/plugin-uploader/
+
+@{
+BASE_URL=$env:KINTONE_BASE_URL
+USERNAME=$env:KINTONE_USERNAME
+PASSWORD=$env:KINTONE_PASSWORD
+PROXY=$env:HTTP_PROXY
+PLUGINS_HOME=$env:PLUGINS_HOME
+}
+
+Get-ChildItem -Path "${env:PLUGINS_HOME}\*.zip" -Recurse -File | `
+  %{ kintone-plugin-uploader $_.FullName; Start-Sleep -Seconds 3 }
